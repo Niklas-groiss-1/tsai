d3697d107b605792d2a3dd65e99e120ce6c3a9be
Niklas-groiss-1 <100076012+Niklas-groiss-1@users.noreply.github.com>
2022-03-31 10:32:46 +0200

    Update layers.py

--- tsai/models/layers.py
+++ tsai/models/layers.py
@@ -181,7 +181,7 @@ class AddCoords1d(Module):
 # Cell
 class ConvBlock(nn.Sequential):
     "Create a sequence of conv1d (`ni` to `nf`), activation (if `act_cls`) and `norm_type` layers."
-    def __init__(self, ni, nf, kernel_size=None, ks=3, stride=5, padding='same', bias=None, bias_std=0.01, norm='Batch', zero_norm=False, bn_1st=True,
+    def __init__(self, ni, nf, kernel_size=None, ks=3, stride=1, padding='same', bias=None, bias_std=0.01, norm='Batch', zero_norm=False, bn_1st=True,
                  act=nn.ReLU, act_kwargs={}, init='auto', dropout=0., xtra=None, coord=False, separable=False,  **kwargs):
         kernel_size = kernel_size or ks
         ndim = 1
